7f3f5c70460cea0740add305aa7b0e0576e45563
Evan Garrett <evan@fitfox.de>
2018-12-05 11:12:50 +0100

    Fail later

--- fail.js
+++ fail.js
@@ -5,6 +5,6 @@ const rejectThatPromise = () => {
 }
 
 
-rejectThatPromise();
+setTimeout(rejectThatPromise, 3000);
 
 // The purpose here is to create an importable module that will return a rejected promise, so that it can be tested for cross origin scripting errors in Sentry.
