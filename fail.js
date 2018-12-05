6c13a8ff95c59e797becc184185d9a5de0faa413
Evan Garrett <evan@fitfox.de>
2018-12-05 11:06:46 +0100

    Failure was successful

---
+++ fail.js
@@ -0,0 +1,10 @@
+const rejectThatPromise = () => {
+	return new Promise((resolve, reject) => {
+		reject('BAD!');
+	})
+}
+
+
+rejectThatPromise();
+
+// The purpose here is to create an importable module that will return a rejected promise, so that it can be tested for cross origin scripting errors in Sentry.
